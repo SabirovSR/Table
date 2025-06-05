75a0a58f2f4247643d81fe2fbc02e491c3604226
SabirovSR <Corner1233@yandex.ru>
2025-06-05 13:54:04 +0300

    хеш говна

--- Table/HashTable.h
+++ Table/HashTable.h
@@ -7,20 +7,12 @@ class HashTable : public Table<TKey, TVal>
 {
 public:
     HashTable(int _size) {
-        this->size = _size * 2;
+        this->size = _size * 4;
     }
     
 protected:
     virtual int HashFunc(TKey key)
     {
-        /*const int prime = 50;
-        int hash = 0;
-        while (key > 0) {
-            hash = (hash * prime + key % 10) % this->size;
-            key /= 10;
-        }
-        return hash;*/
-        //return key % 10;
         return key % this->size;
     }
 };
